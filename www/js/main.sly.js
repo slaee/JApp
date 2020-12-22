82a041121547ec4d3fe051af5139a938a5298884
newbietot <nasa.sly14@gmail.com>
2020-12-22 15:05:27 +0700

    update

--- www/js/main.sly.js
+++ www/js/main.sly.js
@@ -1,25 +1,47 @@
-!function (execute) {
-    function AppSystem(worker) {
-        
-        for (; worker.length; ) {
-            worker.shift()();
-            // const fn = worker.shift();
-            // var obj = new fn();
-        }
-    }
+// !function (modules) {
+//     var app = new Framework7();
 
-    AppSystem(execute);
-}([function () {
+//     function AppSystem(worker) {
+
+//         for (var i = 0; worker.length; i++) {
+
+//             // worker.shift();
+//             // const fn = worker.shift();
+//             // var obj = new fn();
+//         }
+//     }
+//     AppSystem(modules);
+
+//     module.exports = app;
+
+// }([function (modile, require, exports) {
     
-}, function () {
+// }, function (module, require, exports) {
 
-}, function () {
-    window.app = document.body;
+// }, function (module, require, exports) {
 
-    setTimeout(function () {
-        app.classList.remove("loading", "splash");
-    }, 7000)
-    document.addEventListener("deviceready", (function(){
-        
-    }),false);
-}]);
+//     window.onload = function() {
+//         document.addEventListener("deviceready", (function(){
+
+//             window.setTimeout(function () {
+//                 document.body.classList.remove("loading", "splash");
+//             }, 7000);
+
+//             system.clearCache((function(e){
+//                 return console.log("clear cache", e)
+//             }), (function(e){
+//                 return console.error("e")
+//             })),
+//             alert("DeviceReadey");
+            
+//             BuildInfo.debug || setTimeout((function(){
+//                 document.body.classList.contains("loading") &&  popAlert("Something went wrong please! Please clear app data and restart the app or wait.")
+//             }), 3e4),
+//             setTimeout((function(){
+//                 document.body.classList.contains("loading") && document.body.setAttribute("alert-message", "This is taking unexepected long time");
+//             }), 1e4);
+//         }));
+//     }
+// }]);
+
+
